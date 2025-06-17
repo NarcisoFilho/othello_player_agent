ccd631fff8d105f50e3f5dfa99afd1f8cf52fe2e
NarcisoFilho <narcisofilho1@hotmail.com>
2025-06-17 12:08:23 -0300

    counting heuristic: added make_move

--- advsearch/belisarius/othello_minimax_count.py
+++ advsearch/belisarius/othello_minimax_count.py
@@ -23,7 +23,8 @@ def make_move(state) -> Tuple[int, int]:
     # Remova-o e coloque uma chamada para o minimax_move (que vc implementara' no modulo minimax).
     # A chamada a minimax_move deve receber sua funcao evaluate como parametro.
 
-    return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
+    #return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
+    return minimax_move(state, 4, evaluate_count)
 
 
 def evaluate_count(state, player:str) -> float:
